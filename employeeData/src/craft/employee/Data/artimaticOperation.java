754593440245a9406da5d24032a4b7c2f4c69e7a
Yibeltaluser <120437921+Yibeltaluser@users.noreply.github.com>
2023-01-18 00:39:02 -0500

    changes to employee data

---
+++ employeeData/src/craft/employee/Data/artimaticOperation.java
@@ -0,0 +1,15 @@
+package craft.employee.Data;
+
+public class artimaticOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a, b, c;
+		a =10;
+		b = 2;
+		c =a+b;
+		System.out.println(a==b)
+	}
+
+}
---
+++ employeeData/src/craft/employee/Data/whileLoop.java
@@ -0,0 +1,16 @@
+package craft.employee.Data;
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// print number from 1 to 50
+		
+		int i = 1;
+		while (i<=50) {
+			
+		}
+
+	}
+
+}
